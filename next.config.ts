f8c03f235db55e032029c69a7cf43448bcade085
Bep Co Ha <nhamha240587@gmail.com>
2026-06-04 16:03:33 +0700

    fix: xóa redirect loop trong next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [{ source: '/', destination: '/khoahocduacamuoi', permanent: false }]
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'qr.sepay.vn' },
